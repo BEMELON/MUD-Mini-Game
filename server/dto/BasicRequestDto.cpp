60e884b649f79e0031287c9def8ea3553bcfa2f3
BEMELON <hgdkim2@naver.com>
2022-12-13 22:53:21 +0900

    refactor: RequestDTO 인터페이스 구축

--- server/dto/BasicRequestDto.cpp
+++ server/dto/BasicRequestDto.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "../header/BasicRequestDto.h"
+
+bool BasicRequestDto::has(const string &key) {
+    return jsonParser.has(key);
+}
+
+void BasicRequestDto::setBody(const string& body) {
+    jsonParser.parse(body);
+}
+
+string BasicRequestDto::getString(const string& key) {
+    return jsonParser.getString(key);
+}
+
+int BasicRequestDto::getInt(const string& key) {
+    return jsonParser.getInt(key);
+}
--- server/interface/IRequestDTO.h
+++ server/interface/IRequestDTO.h
@@ -6,16 +6,17 @@
 #define MUD_SERVER_IREQUESTDTO_H
 #define interface class
 #include "../header/rapidjson/document.h"
+#include "../header/JsonParser.h"
 
 interface IRequestDTO {
 
 protected:
-    rapidjson::Document* document;
 
 public:
-    void setDocument(rapidjson::Document* document) {
-        this->document = document;
-    }
+    virtual void setBody(const std::string& body)  = 0;
+    virtual std::string getString(const std::string& key) = 0;
+    virtual int getInt(const std::string& key) = 0;
+    virtual bool has(const std::string& key) = 0;
 };
 
 #endif //MUD_SERVER_IREQUESTDTO_H
